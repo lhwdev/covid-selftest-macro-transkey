4fa7aa051507f2273c135e35b951357cd53ae187
lhwdev <lhwdev6@outlook.com>
2021-11-11 13:20:25 +0900

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
 	implementation(project(":api-base"))
 	
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.2.2")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
 	testImplementation(kotlin("test"))
 }
